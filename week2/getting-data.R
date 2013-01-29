fa1f95bb7de65a481f543ee485728d6062f83bc6
Simon Cousins <simon.tyler.cousins@gmail.com>
2013-01-29 21:42:35 +0000

    getting data pt

--- week2/getting-data.R
+++ week2/getting-data.R
@@ -1,3 +1,27 @@
 getwd()
 setwd("./week2")
 # getting files from the internet
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/cameras.csv",method="curl")
+list.files("./data")
+# read into memory - csv
+cameraData <- read.table("./data/cameras.csv")
+head(cameraData)
+cameraData <- read.table("./data/cameras.csv",sep=",",header=TRUE)
+head(cameraData)
+cameraData <- read.csv("./data/cameras.csv")
+head(cameraData)
+# read into memory - xlsx
+library(xlsx)
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/camera.xlsx",method="curl")
+cameraData <- read.xlsx2("./data/camera.xlsx",sheetIndex=1)
+head(cameraData)
+# choose a file to read
+cameraData <- read.csv(file.choose())
+# file handling
+con <- file("./data/cameras.csv","r")
+cameraData <- read.csv(con)
+close(con)
+head(cameraData)
+
